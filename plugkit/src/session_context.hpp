0de7f8ceabc4c07f156c9f82657a92f46b247769
Ron Hashimoto <mail@h2so5.net>
2018-04-03 09:57:42 +0900

    session_context

--- plugkit/src/session_context.hpp
+++ plugkit/src/session_context.hpp
@@ -1,16 +1,27 @@
 #ifndef PLUGKIT_SESSION_CONTEXT_H
 #define PLUGKIT_SESSION_CONTEXT_H
 
+#include <memory>
+
 namespace plugkit {
 struct SharedContextWrapper;
+class ConfigMap;
+class Logger;
+using LoggerPtr = std::shared_ptr<Logger>;
 
 class SessionContext {
 public:
   SessionContext();
   ~SessionContext();
+  SharedContextWrapper *context() const;
+  const LoggerPtr &logger() const;
+  void setLogger(const LoggerPtr &logger);
+  const ConfigMap &config() const;
+  void setConfig(const ConfigMap &config);
 
 private:
-  SharedContextWrapper *shared;
+  class Private;
+  std::unique_ptr<Private> d;
 };
 } // namespace plugkit
 
